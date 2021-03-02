a06978c554813a2f25d13362748dafd3f1309f1f
uufukttas <uufukttas@hotmail.com>
2021-03-02 23:38:08 +0300

    Router Componenet was added.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import Navbar from './Components/Navbar'
 import Users from './Components/Users'
 import  AddUser from './Components/AddUser'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+const Home = () => {
+  return <h3>Home Page</h3>
+};
+
+const About = () => {
+  return <h3>About Page</h3>
+}
 
 class App extends Component {
+//   <Navbar 
+//   title = 'Employers'
+// />
+// <AddUser/>
+// <Users />
   render() {
     return (
-      <div className='container'>
-        <Navbar 
-          title = 'Employers'
-        />
-        <AddUser/>
-        <Users />
-      </div>
+      <Router>
+        <div className='container'>
+          <Route exact path='/' component={Home}>
+          </Route>
+          <Route exact path='/about' component={About}>
+          </Route>
+        </div>
+      </Router>
     );
   }
 }
